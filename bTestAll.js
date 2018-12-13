5afd4816b2abe64a6b5d47b25090d58dbdc2319c
Josh <og.ginger@gmail.com>
2018-12-13 08:39:57 +0000

    Updated bTestAll.js: Working copy of bTestAll. Passes all tests.

--- bTestAll.js
+++ bTestAll.js
@@ -1,16 +1,24 @@
 //bTestAll.js: Functional Logic.
 
 define([
-	"underscore"
+	"underscore",
+	"log"
 ], function(
-	_
+	_,
+	log
 ) {
   return function( Input ) {
 	var Result = false;
-	if ( Input !== undefined ) {
-		Result = _.every( Input, function( Test ) {
-			return Test.Run();
-		});
+	try {
+		if ( _.isEmpty( Input ) === false ) {
+		//if the test collection is not empty then...
+			//Run every test.
+			Result = _.every( Input, function( Test ) {
+				return Test.Run();
+			});
+		}
+	} catch ( Error ) {
+		log("Error: " + JSON.stringify( Error ));
 	}
 	return Result;
   };
