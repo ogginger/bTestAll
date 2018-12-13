52b10ccaa84b91067fdf106a39eaea9c874dd7f7
Josh <og.ginger@gmail.com>
2018-12-13 08:39:00 +0000

    Updated test_bTestAll.js: All of the tests are active and pass.

--- test_bTestAll.js
+++ test_bTestAll.js
@@ -8,7 +8,8 @@ define([
   "tests/bTestAll_MultiplePassingTests_ReturnsTrue",
   "tests/bTestAll_FailingTest_ReturnsFalse",
   "tests/bTestAll_OneTestFails_ReturnsFalse",
-	"tests/bTestAll_NoArguments_ReturnsFalse"
+	"tests/bTestAll_NoArguments_ReturnsFalse",
+	"tests/bTestAll_TestThrowsError_ReturnsFalse"
 ], function(
   TestSuite,
   log,
@@ -17,18 +18,20 @@ define([
   bTestAll_MultiplePassingTests_ReturnsTrue,
   bTestAll_FailingTest_ReturnsFalse,
   bTestAll_OneTestFails_ReturnsFalse,
-	bTestAll_NoArguments_ReturnsFalse
+	bTestAll_NoArguments_ReturnsFalse,
+	bTestAll_TestThrowsError_ReturnsFalse
 ) {
   return TestSuite.extend({
     "initialize": function() {
       log("test_bTestAll initialized successfully!");
       var xTestSuite = this;
-      xTestSuite.set( "MethodUnderTest", "bTestAll" );
-        xTestSuite.add( bTestAll_PassingTest_ReturnsTrue );
+      xTestSuite.set( "MethodUnderTest", "bTestAll" );	
+	xTestSuite.add( bTestAll_PassingTest_ReturnsTrue );
 	xTestSuite.add( bTestAll_MultiplePassingTests_ReturnsTrue );
 	xTestSuite.add( bTestAll_FailingTest_ReturnsFalse );
 	xTestSuite.add( bTestAll_OneTestFails_ReturnsFalse );
 	xTestSuite.add( bTestAll_NoArguments_ReturnsFalse );
+	xTestSuite.add( bTestAll_TestThrowsError_ReturnsFalse );
       xTestSuite.test();
     }
   });
